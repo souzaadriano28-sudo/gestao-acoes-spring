ffe24887eb2a1187e77b73f1315fa5dfca14438b
Adriano <souzaadriano28@yahoo.com.br>
2026-04-27 21:58:16 -0300

    chore: commit inicial do projeto Spring Boot

---
+++ src/main/java/com/trabalho/gestao_acoes/GestaoAcoesApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.gestao_acoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAcoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoAcoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabalho/gestao_acoes/GestaoAcoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.gestao_acoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoAcoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
